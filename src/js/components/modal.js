d8b0e147405421223c693e8b4749e767bd99ec0f
Marselina-you <marselina88@list.ru>
2023-10-21 08:07:00 +0300

    form-auto

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,28 +1,46 @@
-const btns = document.querySelectorAll('.help-button');
-const modalOverlay = document.querySelector('.modal-help-overlay ');
-const modals = document.querySelectorAll('.modal-help');
+const btnHelp = document.querySelector('.help-button');
+const help = document.querySelector('.modal-help');
+
+const btns = document.querySelectorAll('.auto-button');
+const modalOverlay = document.querySelector('.modal-auto-overlay ');
+const modals = document.querySelectorAll('.modal-auto');
+const close = document.querySelector('.modal-auto__close-img');
+console.log(close)
+
+btnHelp.addEventListener('click', () => {
+  help.classList.toggle('modal-help--visible');
+});
+
+document.addEventListener('click', (e) => {
+  console.log(e.target)
+  if (!e.target.classList.contains('modal-help') && e.target.closest('.modal-help') && !e.target.classList.contains('help-button')) {
+   help.classList.remove('modal-help--visible');
+  }
+});
 
 btns.forEach((el) => {
 	el.addEventListener('click', (e) => {
 		let path = e.currentTarget.getAttribute('data-path');
 
 		modals.forEach((el) => {
-			el.classList.remove('modal-help--visible');
+			el.classList.remove('modal-auto--visible');
 		});
 
-		document.querySelector(`[data-target="${path}"]`).classList.add('modal-help--visible');
-		modalOverlay.classList.add('modal-help-overlay--visible');
+		document.querySelector(`[data-target="${path}"]`).classList.add('modal-auto--visible');
+		modalOverlay.classList.add('modal-auto-overlay--visible');
 	});
 });
 
 modalOverlay.addEventListener('click', (e) => {
 
 
-	if (e.target == modalOverlay) {
-    console.log(modalOverlay)
-		modalOverlay.classList.remove('modal-help-overlay--visible');
+	if (e.target == close  || e.target == modalOverlay) {
+
+		modalOverlay.classList.remove('modal-auto-overlay--visible');
 		modals.forEach((el) => {
-			el.classList.remove('modal-help--visible');
+			el.classList.remove('modal-auto--visible');
 		});
 	}
 });
+
+
